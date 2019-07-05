c67810d65c886ec4b2b23bf4c27309fc6da73d46
febil <febil.tvr@gmail.com>
2019-07-05 21:18:45 +0530

    arrange cards in the hand

--- lib/poker_play.ex
+++ lib/poker_play.ex
@@ -1,7 +1,50 @@
 defmodule PokerPlay do
+  alias PokerPlay.Card
+
   @moduledoc """
   Documentation for PokerPlay.
   """
+
+  @doc """
+  Compare the input hands white and black
+  """
+  require IEx
+
+  def compare(hand1, hand2) do
+    white =
+      hand1
+      |> Card.format_input()
+      |> Enum.map(fn x -> String.split(x, "", trim: true) end)
+      |> Enum.map(fn x -> Card.init(Enum.at(x, 0), Enum.at(x, 1)) end)
+
+    black =
+      hand2
+      |> Card.format_input()
+      |> Enum.map(fn x -> String.split(x, "", trim: true) end)
+      |> Enum.map(fn x -> Card.init(Enum.at(x, 0), Enum.at(x, 1)) end)
+
+    white
+    # find_hand_type(white)
+    # find_hand_type(black)
+  end
+
+  def find_hand_type(cards) do
+    cards
+    |> arrange_cards()
+
+    # |> hand_type(cards)
+  end
+
+  def arrange_cards(cards) do
+    cards
+    |> Enum.group_by(& &1.int_value)
+    |> Enum.sort_by(fn {int_value, _items} -> int_value end)
+    |> Enum.sort_by(fn {_int_value, items} -> Kernel.length(items) end)
+    |> Enum.reverse()
+    |> Enum.map(fn {_int_value, items} -> Enum.map(items, & &1.value) end)
+  end
+
+  # defp split_
 
   @doc """
   Hello world.
@@ -12,7 +55,7 @@ defmodule PokerPlay do
       :world
 
   """
-  def hello do
-    :world
-  end
+  # def hello do
+  #   :world
+  # end
 end
--- test/poker_play_test.exs
+++ test/poker_play_test.exs
@@ -11,6 +11,7 @@ defmodule PokerPlayTest do
 
   test "format the given input" do
     assert Card.format_input("Black: 2H 3D 5S 9C KD") == ["2H", "3D", "5S", "9C", "KD"]
+    assert Card.format_input("White: 2H 3D 5S 9C KD") == ["2H", "3D", "5S", "9C", "KD"]
   end
 
   test "initialize input hand to Card struct" do
@@ -18,4 +19,86 @@ defmodule PokerPlayTest do
     assert PokerPlay.Card.init("T", "S") == %PokerPlay.Card{value: "T", suit: "S", int_value: 10}
     assert PokerPlay.Card.init("K", "D") == %PokerPlay.Card{value: "K", suit: "D", int_value: 13}
   end
+
+  test "Arrange cards of a hand for High Card" do
+    cards =
+      "Black: 2H 3D 5S 9C KD"
+      |> Card.format_input()
+      |> Enum.map(fn x -> String.split(x, "", trim: true) end)
+      |> Enum.map(fn x -> Card.init(Enum.at(x, 0), Enum.at(x, 1)) end)
+
+    assert PokerPlay.arrange_cards(cards) == [["K"], ["9"], ["5"], ["3"], ["2"]]
+  end
+
+  test "Arrange cards of a hand for comparison pair" do
+    cards =
+      "Black: 2H 2D 5S 9C KD"
+      |> Card.format_input()
+      |> Enum.map(fn x -> String.split(x, "", trim: true) end)
+      |> Enum.map(fn x -> Card.init(Enum.at(x, 0), Enum.at(x, 1)) end)
+
+    assert PokerPlay.arrange_cards(cards) == [["2", "2"], ["K"], ["9"], ["5"]]
+  end
+
+  test "Arrange cards of a hand for comparison two pair" do
+    cards =
+      "Black: 2H 2D 5S 5C KD"
+      |> Card.format_input()
+      |> Enum.map(fn x -> String.split(x, "", trim: true) end)
+      |> Enum.map(fn x -> Card.init(Enum.at(x, 0), Enum.at(x, 1)) end)
+
+    assert PokerPlay.arrange_cards(cards) == [["5", "5"], ["2", "2"], ["K"]]
+  end
+
+  test "Arrange cards of a hand for comparison three of kind" do
+    cards =
+      "Black: 2H 2D 2S 9C KD"
+      |> Card.format_input()
+      |> Enum.map(fn x -> String.split(x, "", trim: true) end)
+      |> Enum.map(fn x -> Card.init(Enum.at(x, 0), Enum.at(x, 1)) end)
+
+    assert PokerPlay.arrange_cards(cards) == [["2", "2", "2"], ["K"], ["9"]]
+  end
+
+  test "Arrange cards of a hand for comparison four of kind " do
+    cards =
+      "Black: 2H 2D 2S 2C KD"
+      |> Card.format_input()
+      |> Enum.map(fn x -> String.split(x, "", trim: true) end)
+      |> Enum.map(fn x -> Card.init(Enum.at(x, 0), Enum.at(x, 1)) end)
+
+    assert PokerPlay.arrange_cards(cards) == [["2", "2", "2", "2"], ["K"]]
+  end
+
+  test "Arrange cards of a hand for comparison of Straight " do
+    cards =
+      "Black: 2H 3D 4S 6C 5D"
+      |> Card.format_input()
+      |> Enum.map(fn x -> String.split(x, "", trim: true) end)
+      |> Enum.map(fn x -> Card.init(Enum.at(x, 0), Enum.at(x, 1)) end)
+
+    assert PokerPlay.arrange_cards(cards) == [["6"], ["5"], ["4"], ["3"], ["2"]]
+  end
+
+  test "Arrange cards of a hand for comparison of Flush and StraightFlush " do
+    cards =
+      "Black: 2H 3H 4H 6H 5H"
+      |> Card.format_input()
+      |> Enum.map(fn x -> String.split(x, "", trim: true) end)
+      |> Enum.map(fn x -> Card.init(Enum.at(x, 0), Enum.at(x, 1)) end)
+
+    assert PokerPlay.arrange_cards(cards) == [["6"], ["5"], ["4"], ["3"], ["2"]]
+  end
+
+  test "Arrange cards of a hand for comparison of FullHouse " do
+    cards =
+      "Black: 2H 3D 2S 2C 3D"
+      |> Card.format_input()
+      |> Enum.map(fn x -> String.split(x, "", trim: true) end)
+      |> Enum.map(fn x -> Card.init(Enum.at(x, 0), Enum.at(x, 1)) end)
+
+    assert PokerPlay.arrange_cards(cards) == [["2", "2", "2"], ["3", "3"]]
+  end
+
+  # test
 end
