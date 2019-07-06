0f824eec54da3f6eeee3c09218ce3d9c3efc2116
febil <febil.tvr@gmail.com>
2019-07-07 01:24:16 +0530

    find the full house kind of card from the hand

---
+++ lib/full_house.ex
@@ -0,0 +1,15 @@
+defmodule PokerPlay.FullHouse do
+  def check(arranged_card_values) do
+    find_pair_at(arranged_card_values, 0, 3) && find_pair_at(arranged_card_values, 1, 2)
+  end
+
+  @doc """
+  this function will return true if it find a pair at the given index
+  """
+  def find_pair_at(values, index_position, length_of_a_pair) do
+    values
+    |> Enum.at(index_position)
+    |> Kernel.length()
+    |> Kernel.==(length_of_a_pair)
+  end
+end
